a1efbbfa0f2b36bbd4c28d87a4759894c940e7a0
jessika <jessikafujimura@gmail.com>
2024-02-12 22:45:35 -0300

    feat: create endpoint de updateCustomer

--- src/main/java/me/dio/credit/application/system/controller/CustomerController.kt
+++ src/main/java/me/dio/credit/application/system/controller/CustomerController.kt
@@ -1,10 +1,13 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.dto.CustomerUpdateDto
 import me.dio.credit.application.system.dto.CustomerView
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.service.impl.CustomerService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -32,5 +35,15 @@ class CustomerController(
     @DeleteMapping("/{id}")
     fun deleteCustomerById(@PathVariable id: Long){
         customerService.delete(id);
+    }
+
+    @PatchMapping
+    fun updateCustomer(@RequestParam(value = "customerId") id: Long,
+                       @RequestBody customerUpdateDto: CustomerUpdateDto
+    ): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        val toEntity = customerUpdateDto.toEntity(customer)
+        val entitySaved = this.customerService.save(toEntity);
+        return CustomerView(entitySaved)
     }
 }
